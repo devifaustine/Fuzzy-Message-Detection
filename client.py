4498a47c7422cde98f491cb2be07c5cde38455b5
devifaustine <devifaustine.df@gmail.com>
2022-02-18 17:08:04 +0100

    Add stubs for client.py

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
 # implement client of FMD scheme
+from FMD2 import PublicKey, SecretKey, KeyGen
+from ecpy.curves import Curve
+
+
+class Client:
+    pk: PublicKey
+    sk: SecretKey
+
+    def __init__(self):
+        # TODO: find out which curve to use! 
+        curve = Curve.get_curve('secp256k1')
+        sk, pk = KeyGen(curve)
+
+    def simulate(self):
+        pass  # TODO: implement me!
