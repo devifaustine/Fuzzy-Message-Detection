a9c706cb632fdbd3784fcd7e5a944300234f2170
devifaustine <devifaustine.df@gmail.com>
2022-02-18 16:21:04 +0100

    Add KeyGen() implementation and rough draft for FMD2

---
+++ FMD2.py
@@ -0,0 +1,54 @@
+# implementation of whole FMD2 primitives
+import math
+import random
+from ecpy.curves import Curve
+from ecpy.keys import ECPrivateKey
+
+
+class PublicKey:
+    numKeys: int
+    pubKeys: list
+
+    def __init__(self, num=0, pub=None):
+        if pub is None:
+            pub = []
+        self.numKeys = num
+        self.pubKeys = pub
+
+
+class SecretKey:
+    numKeys: int
+    secKeys: list
+
+    def __init__(self, num=0, sec=None):
+        if sec is None:
+            sec = []
+        self.numKeys = num
+        self.secKeys = sec
+
+
+def KeyGen(curve: Curve, numKeys=15):
+    sk = SecretKey(numKeys)
+    pk = PublicKey(numKeys)
+
+    for i in range(numKeys):
+        randomNum = random.randrange(0, 9999999) # TODO: find out range for Zq!
+        sk.secKeys[i] = ECPrivateKey(randomNum, curve)
+        pk.pubKeys[i] = sk.secKeys[i].get_public_key()
+
+    return sk, pk
+
+
+def Extract(sk: SecretKey, p: float):
+    # p = false positive rate in form 2^(-n) with 0 <= n <= gamma (15).
+    n = int(math.log(1 / p, 2))
+    result = []
+    for i in range(n):
+        result.append(sk.secKeys[i])
+    return result
+
+
+def Flag(pk: PublicKey, curve: Curve):
+    pubKey = pk.pubKeys
+    r = random.randrange(0, 99999999) # TODO: find out range for Zq!
+    pass # TODO: implement me!
---
+++ KeyGen.py
@@ -0,0 +1,63 @@
+# implement client of FMD scheme
+from ctypes import c_uint32
+
+from ecpy.curves import Curve, Point
+from ecpy.keys import ECPublicKey, ECPrivateKey
+from ecpy.ecdsa import ECDSA
+import random
+from dataclasses import dataclass
+import ctypes
+
+
+class GroupElement:
+    X: int
+    Y: int
+
+
+class PublicKey:
+    numKeys: int
+    pubKeys: list
+
+    def __init__(self, num=0, pub=None):
+        if pub is None:
+            pub = []
+        self.numKeys = num
+        self.pubKeys = pub
+
+
+class SecretKey:
+    numKeys: int
+    secKeys: list
+    prob: ctypes.c_uint32[int]
+
+    def __init__(self, num=0, sec=None, prob=0):
+        if sec is None:
+            sec = []
+        self.numKeys = num
+        self.secKeys = sec
+        self.prob = prob
+
+
+# TODO: find out which elliptic curve is used in implementation!
+cv = Curve.get_curve('secp256k1')
+# numKeys / gamma in the research paper is determined to be  15
+numKeys = 15
+
+
+# TODO: implement KeyGen() algorithm FMD2
+def keyGen(curve: Curve, numKeys: int):
+    #  Public Key
+    pub = PublicKey(numKeys, [GroupElement] * numKeys)
+
+    # Secret Key
+    prob: c_uint32[int] = ctypes.c_uint32(numKeys)
+    priv = SecretKey(numKeys, [GroupElement] * numKeys, prob)
+
+    # generate each public and secret key
+    for i in range(numKeys):
+        randomNum = random.randrange(0, 9999999)  # di implementasi randomNum nya gede (bigInt)
+        priv.secKeys[i] = ECPrivateKey(randomNum, curve)
+        pub.pubKeys[i] = priv.secKeys[i].get_public_key()
+    print(priv.secKeys)
+    print(pub.pubKeys)
+    return priv.secKeys, pub.pubKeys
