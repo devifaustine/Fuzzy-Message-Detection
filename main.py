be5ef80b79f7e35f18891a2677c35ea39cb6fde7
devifaustine <devifaustine.df@gmail.com>
2022-02-19 23:57:58 +0100

    Update main, run() executed in try block#

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from server import Server
 
 # number of clients produced can be changed
 server = Server(20)
-# false positive rate can be changed
-server.run(1/8)
+
+try:
+    # false positive rate can be changed
+    server.run(1/8)
+except AssertionError:
+    print("FMD2 experiment was unsuccessful!")
+else:
+    print("FMD2 experiment was successful!")
+finally:
+    print("Experiment done :), try with other values!")
