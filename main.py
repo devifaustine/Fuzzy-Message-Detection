e437ca164d7920ed51f355b7b3ca0135e0b0b698
devifaustine <devifaustine.df@gmail.com>
2022-03-01 00:39:56 +0100

    Add for loop in main so experiment can be run many times

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from server import Server
 # instantiate a server, number of clients produced can be changed
 server = Server(200)
 
-try:
-    # run the server/FMD2 experiment,
-    # false positive rate can be changed
-    server.run(1/2)
-except AssertionError:
-    # can be triggered because there are false negatives,
-    # p is not in the right form or false positive rate is false
-    print("FMD2 experiment was unsuccessful!")
-    print("Experiment done :(, try again!")
-else:
-    print("FMD2 experiment was successful!")
-    print("Experiment done :), try with other values!")
+# run this experiment n times
+for i in range(5):
+    try:
+        # run the server/FMD2 experiment,
+        # false positive rate can be changed
+        server.run(1 / 2)
+    except AssertionError:
+        # can be triggered because there are false negatives,
+        # p is not in the right form or false positive rate is false
+        print("FMD2 experiment "+str(i+1)+" was unsuccessful!")
+        print("Experiment done :(, try again!")
+    else:
+        print("FMD2 experiment "+str(i+1)+" was successful!")
+        print("Experiment done :), try with other values!")
