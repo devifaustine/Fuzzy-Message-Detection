fdac401d71a45ddb9f40291aa9666d4cff1dec92
devifaustine <devifaustine.df@gmail.com>
2022-03-01 00:49:08 +0100

    Add success rate of experiment in main

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from server import Server
 # instantiate a server, number of clients produced can be changed
 server = Server(200)
 
+# value keeps track of successful experiments
+x = 0
+n = 10
+
 # run this experiment n times
-for i in range(5):
+for i in range(n):
     try:
         # run the server/FMD2 experiment,
         # false positive rate can be changed
@@ -15,6 +19,11 @@ for i in range(5):
         # p is not in the right form or false positive rate is false
         print("FMD2 experiment "+str(i+1)+" was unsuccessful!")
         print("Experiment done :(, try again!")
+        print("")
     else:
+        x += 1
         print("FMD2 experiment "+str(i+1)+" was successful!")
         print("Experiment done :), try with other values!")
+        print("")
+
+print("Experiment was "+str(x/n*100)+"% successful!")
